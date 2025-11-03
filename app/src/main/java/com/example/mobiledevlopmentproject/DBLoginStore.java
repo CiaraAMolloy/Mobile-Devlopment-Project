89d355ee2959de7558b18e93ad3f0ff5b1178fe6
ouszin <sara.oliszewska65@gmail.com>
2025-11-03 13:23:27 +0000

    added in details and a new DB for login credentials

---
+++ app/src/main/java/com/example/mobiledevlopmentproject/DBLoginStore.java
@@ -0,0 +1,6 @@
+package com.example.mobiledevlopmentproject;
+
+public class DBLoginStore {
+    //Will contain username, email, password.
+    //TODO: figure out how to link my data with Ciara's data.
+}
